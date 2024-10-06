b2d5d4c6efdd839f40f917cce9d6d4f6386f42a7
Manjari Jain <manjarijain98@gmail.com>
2024-10-06 17:51:18 +0530

    initial spring boot setup

---
+++ src/main/java/com/protoc/proto_java/ProtoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.protoc.proto_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/protoc/proto_java/ProtoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protoc.proto_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
